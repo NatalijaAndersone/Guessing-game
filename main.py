6fcfcd7d7e9b2d72c0832dde7cd7cf5399cb288a
Natalija Andersone <74709560+NatalijaAndersone@users.noreply.github.com>
2021-10-05 22:04:53 +0300

    Pievienota žurnalēšana

--- main.py
+++ main.py
@@ -1,12 +1,20 @@
-import random
+import random, logging
 number = random.randint(1, 10)
 
+
+logging.basicConfig(filename='guess.log', level=logging.DEBUG, format='%(asctime)s %(message)s')
+
+
 player_name = input("Hello, what is your name? ")
+logging.info('****NEW GAME****')
+logging.info('Player has to guess: ' + str(number))
+logging.info('Player name ' + player_name)
 number_of_guesses = 0
 print('I\'m glad to meet you! {} \nLet\'s play a game with you, I will think a number between 1 and 10 then you will guess, alright? \nDon\'t forget! You have only 3 chances so guess:'.format(player_name))
 
 while number_of_guesses < 3:
     guess = int(input())
+    logging.info('Player guessed: ' + str(guess))
     number_of_guesses += 1
     if guess < number:
         print('Your estimate is too low, go up a little!')
@@ -16,5 +24,7 @@ while number_of_guesses < 3:
         break
 if guess == number:
     print( 'Congratulations {}, you guessed the number in {} tries!'.format(player_name, number_of_guesses))
+    logging.info('Player won with ' + str(number_of_guesses) + ' number of guesses')
 else:
     print('Close but no cigar, you couldn\'t guess the number. \nWell, the number was {}.'.format(number))
+    logging.info('Player lost')
