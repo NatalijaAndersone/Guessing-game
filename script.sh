28de0c15b8b13c25db179bb76a16f3bb079a0e08
Natalija Andersone <natalija.andersone@va.lv>
2021-11-23 20:48:57 +0200

    Papildināts script fails

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-echo "Script for preparing the development environment"
+echo "Script for checking the configuration files"
 echo "------------------------------------------------"
 
-echo "Checking if config.ini exists in the current working dir -->"
+echo "Checking if config.ini exists -->"
 if test -f "C:\Users\natan\PycharmProjects\Guessing-game\config.ini"; then
     echo "exists"
 else
@@ -11,27 +11,12 @@ else
 fi
 echo "------------------------------------------------"
 
-echo "Checking if log_migrate_db.yaml exists in the current working dir -->"
-if test -f "log_migrate_db.yaml"; then
+echo "Checking if log_migrate_db.yaml exists-->"
+if test -f "C:\Users\natan\PycharmProjects\Guessing-game\log\log_migrate_db.yaml"; then
     echo "exists"
 else
-	if [ $? -eq 0 ]; then echo "OK"; else echo "Problem copying log_migrate_db.yaml file"; exit 1; fi
+	if [ $? -eq 0 ]; then echo "OK"; else echo "No log_migrate_db.yaml file"; exit 1; fi
 fi
 echo "------------------------------------------------"
 
-echo "Getting python3 executable loc"
-python_exec_loc=$(which python3)
-if [ $? -eq 0 ]; then echo "OK"; else echo "Problem getting python3 exec location"; exit 1; fi
-echo "$python_exec_loc"
-echo "------------------------------------------------"
-
-echo "Running config tests"
-$python_exec_loc test_config.py
-if [ $? -eq 0 ]; then echo "OK"; else echo "Configuration test FAILED"; exit 1; fi
-echo "------------------------------------------------"
-
-echo "Running DB migrations"
-$python_exec_loc migrate_db.py
-if [ $? -eq 0 ]; then echo "OK"; else echo "DB migration FAILED"; exit 1; fi
-echo "------------------------------------------------"
-echo "ALL SET UP! YOU ARE READY TO CODE"
+echo "ALL SET UP!"
